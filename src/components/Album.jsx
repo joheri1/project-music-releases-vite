96cbe6cc8c2211c4078a81fd61efd481423f6f6a
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-10-13 22:38:06 +0200

    Create album-art-container for styling the buttons on top of the AlbumArt

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -6,8 +6,10 @@ import AlbumIcons from './AlbumIcons'
 const Album = ({ albumData }) => {
   return (
     <div className="album-card">
-      <AlbumArt image={albumData.images[0].url} />
-      <AlbumIcons />
+      <div className="album-art-container">
+        <AlbumArt image={albumData.images[0].url} />
+        <AlbumIcons />
+        </div>
       <div className="album-title">
         <AlbumTitle name={albumData.name} url={albumData.external_urls.spotify} />
       </div>
