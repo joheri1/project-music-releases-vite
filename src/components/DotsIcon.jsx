2fc0509cb623e01a5373dec7d8254cc3813a31c9
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-11-06 23:06:42 +0100

    Put Helvetica first in the font stack

--- src/components/DotsIcon.jsx
+++ src/components/DotsIcon.jsx
@@ -1,9 +1,11 @@
 const DotsIcon = () => {
   return (
     <svg className="icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="2.5" cy="12.5" r="2.5" />
-      <circle cx="12" cy="12.5" r="2.5" />
-      <circle cx="21.5" cy="12.5" r="2.5" />
+      <g fill="#FFFFFF">
+        <circle cx="2.5" cy="12.5" r="2.5" />
+        <circle cx="12" cy="12.5" r="2.5" />
+        <circle cx="21.5" cy="12.5" r="2.5" />
+        </g>
     </svg>
   )
 }
