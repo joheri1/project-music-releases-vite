c18b056fb7c40c6d03c5ae08b191de13f7ce97ff
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-11-06 22:33:58 +0100

    Update the components for each icon by replacing <img> with to enable CSS styling and scaling

--- src/components/DotsIcon.jsx
+++ src/components/DotsIcon.jsx
@@ -1,9 +1,13 @@
-import dotsIcon from '../assets/icons/dots.svg'
-
 const DotsIcon = () => {
   return (
-    <img src={dotsIcon} alt="More" className="icon more-icon" />
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.933333 16.933334">
+  <g fill="#FFFFFF">
+    <circle cx="2.5" cy="12.5" r="2.5"/>
+    <circle cx="12" cy="12.5" r="2.5"/>
+    <circle cx="21.5" cy="12.5" r="2.5"/>
+  </g>
+</svg>
   )
 }
+export default DotsIcon;
 
-export default DotsIcon
