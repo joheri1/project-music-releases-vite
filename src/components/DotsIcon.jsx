43222624192d4297ffe1a2babe3c614b765fdc98
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-11-06 22:36:27 +0100

    Add ClassName=icon to each icon component.

--- src/components/DotsIcon.jsx
+++ src/components/DotsIcon.jsx
@@ -1,6 +1,6 @@
 const DotsIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.933333 16.933334">
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.933333 16.933334">
   <g fill="#FFFFFF">
     <circle cx="2.5" cy="12.5" r="2.5"/>
     <circle cx="12" cy="12.5" r="2.5"/>
